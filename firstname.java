3e34f76f8bb253d19ddbc29377d8e4928401fef9
MOSESDESHMUKH <130032987+MOSESDESHMUKH@users.noreply.github.com>
2023-11-26 17:52:33 +0530

    Create firstname.java

---
+++ firstname.java
@@ -0,0 +1,14 @@
+public class UniqueFirstNames {
+
+    public static void main(String[] args) {
+        String[] uniqueFirstNames = {
+            "Liam", "Olivia", "Noah", "Emma", "Oliver", "Ava", "Elijah", "Sophia", "Lucas", "Isabella",
+            "Mia", "Jackson", "Aiden", "Aria", "Grayson", "Amelia", "Sebastian", "Harper", "Ethan", "Evelyn"
+        };
+
+        // Print the unique first names
+        for (String firstName : uniqueFirstNames) {
+            System.out.println(firstName);
+        }
+    }
+}
